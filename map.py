3519e2711702f671f751f29f2fdc182282c75bfd
Miron <miron@users.noreply.github.com>
2023-01-04 04:27:19 +0000

    red player token, boundaries check

--- map.py
+++ map.py
@@ -5,6 +5,9 @@ def main(stdscr):
     """wrapped for debugging, will be replaced by curses.endwin()"""
     curses.curs_set(False)  # hide the cursor
     curses.init_color(0, 0, 0, 0)
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+    player_color = curses.color_pair(1)
+
 
     # Set up the map
     map_matrix = [
@@ -31,7 +34,7 @@ def main(stdscr):
         for y_axis, row in enumerate(map_matrix):
             for x_axis, cell in enumerate(row):
                 stdscr.addch(y_axis, x_axis, cell)
-        stdscr.addch(player_y, player_x, '@')
+        stdscr.addch(player_y, player_x, '@', player_color)
         key = stdscr.getch()
         new_x = player_x
         new_y = player_y
@@ -45,11 +48,12 @@ def main(stdscr):
             new_x += 1
         elif key == ord('q'):
             break
-        # Check for collision with walls
-        if map_matrix[new_y][new_x] == '#':
-            continue
-        # Update the player position
-        player_x = new_x
-        player_y = new_y
+        # Check if the new position is within the bounds of the map
+        if (new_x >= 0 and new_x < len(map_matrix[0]) and new_y >= 0 and new_y < len(map_matrix)):
+            # Check for collision with walls 
+            if map_matrix[new_y][new_x] !='#':
+                # Update the player position
+                player_x = new_x
+                player_y = new_y
 
 curses.wrapper(main)
