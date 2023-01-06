5c9b62dd09e3aad0220752321bbd4bbfe1a964a8
Miron <miron@users.noreply.github.com>
2023-01-06 23:02:59 +0100

    dict of lists

--- sheet.py
+++ sheet.py
@@ -1,5 +1,5 @@
-characters_sheet = [
-    "Forty",             # Handle  
+characters_sheet = {
+    "Forty": [           # Handle  
     "Rockerboy",         # Role
 
     [ 5,                 # INT
@@ -96,4 +96,102 @@ characters_sheet = [
     "ascii representation of character",        # Portrait 
     "Notes"                                    # Notes
     ]
+
+    "Forty": [             # Handle  
+    "Rockerboy",         # Role
+
+    [ 5,                 # INT
+      0,                 # Accounting
+      0,                 # Bureaucracy
+      0,                 # Business
+      6,                 # Composition
+      0,                 # Conceal/Reveal Object
+      0,                 # Criminology
+      0,                 # Cryptography
+      0,                 # Deduction
+      2,                 # Education
+      0,                 # Library Search
+      4,                 # Local Expert
+      2,                 # Perception
+      0,                 # Tactics
+      0 ],               # Tracking
+
+    [ 6,                 # REF
+      0,                 # Drive Land Vehicle
+      6,                 # Handgun
+      0 ],               # Shoulder Arms
+
+    [ 7,                 # DEX
+      2,                 # Athletics
+      6,                 # Brawling
+      6,                 # Evasion
+      6,                 # Melee Weapon
+      2 ],               # Stealth
+
+    [ 5,                 # TECH
+      0,                 # Electronics/Security Tech
+      6,                 # First Aid
+      0,                 # Forgery
+      0,                 # Paramedic
+      0,                 # Photography/Film
+      0,                 # Pick Lock
+      0,                 # Pick Pocket
+      6 ],               # Play Instrument
+
+    [ 7,                 # COOL
+      0,                 # Acting
+      0,                 # Bribery
+      0,                 # Interrogation
+      6,                 # Persuation
+      6,                 # Streetwise
+      0,                 # Trading
+      4 ],               # Wardrobe & Style
+
+    [ 8,                 # WILL
+      2,                 # Concentration
+      0 ],               # Resist Torture/Drugs
+
+      5,                 # LUCK
+      7,                 # MOVE
+      3,                 # BODY
+
+    [ 6,                 # EMP
+      2,                 # Conversation
+      6 ],               # Human Perception
+
+      40,                # HP
+      20,                # seriously_wounded
+      3,                 # death_save
+
+    [ "Light Armorjack", # Armor
+      11 ],                # SP
+
+    [ "Very Heavy Pistol", # Weapon 1
+      "4d6",                # DMG
+      8,                # Ammo
+      1,                # ROF
+      "You have 16 rounds of extra ammunition" ], # Notes
+    [ "Heavy Melee Weapon",                       # Weapon 2
+      "3d6",                                      # DMG
+      None,                                       # Ammo
+      2,                                          # ROF
+      "A sword or a baseball bat. Your choice." ], # Notes
+    """Charismatic Impact
+    You know when someone is a fan and            
+    receive a +2 to any EMP or COOL 
+    based Skill Check made against them,
+    including Facedowns.""",                         # Role Ability
+    """Internal Agent
+    You have a self-adaptive AI-powered Smart Phone in your head,
+    controlled entirely by voice command.""",         # Cyberware 1
+    """Paint Editor Chipware
+    You can shut off your pain receptors, ignoring you to ignore the -2 to all
+    Checks granted by the Seriously Wounded Wound State.""", # Cyberware 2
+    {"Musical Instrument": "Player's choice",
+    "Pocket Amp": "Amplifies musical instrument",
+    "Glow Paint": "Glow in the dark spraypaint",
+    "Video Camera": "Records up to 12 hours"}, # Gear
+    "ascii representation of character",        # Portrait 
+    "Notes" ]                                  # Notes
+}
 
