d2fdd7527337bcc71af3271caa468a6644fd1781
Miron <miron@users.noreply.github.com>
2023-01-07 06:09:13 +0100

    added solo

--- sheet.py
+++ sheet.py
@@ -1,197 +1,175 @@
 characters_sheet = {
-    "Forty": [           # Handle  
-    "Rockerboy",         # Role
+    "Forty": (                  # Handle  
+    "Rockerboy",                # Role
+ 
+    (5,                         # INT
+    0,                          # Accounting
+    0,                          # Bureaucracy
+    0,                          # Business
+    6,                          # Composition
+    0,                          # Conceal/Reveal Object
+    0,                          # Criminology
+    0,                          # Cryptography
+    0,                          # Deduction
+    2,                          # Education
+    0,                          # Library Search
+    4,                          # Local Expert
+    2,                          # Perception
+    0,                          # Tactics
+    0),                         # Tracking
 
-    [ 5,                 # INT
-      0,                 # Accounting
-      0,                 # Bureaucracy
-      0,                 # Business
-      6,                 # Composition
-      0,                 # Conceal/Reveal Object
-      0,                 # Criminology
-      0,                 # Cryptography
-      0,                 # Deduction
-      2,                 # Education
-      0,                 # Library Search
-      4,                 # Local Expert
-      2,                 # Perception
-      0,                 # Tactics
-      0 ],               # Tracking
+    (6,                         # REF
+    0,                          # Drive Land Vehicle
+    6,                          # Handgun
+    0),                         # Shoulder Arms
 
-    [ 6,                 # REF
-      0,                 # Drive Land Vehicle
-      6,                 # Handgun
-      0 ],               # Shoulder Arms
+    (7,                         # DEX
+    2,                          # Athletics
+    (6, 1),                     # Brawling, 1d6
+    6,                          # Evasion
+    6,                          # Melee Weapon
+    2),                         # Stealth
 
-    [ 7,                 # DEX
-      2,                 # Athletics
-      6,                 # Brawling
-      6,                 # Evasion
-      6,                 # Melee Weapon
-      2 ],               # Stealth
+    (5,                         # TECH
+    0,                          # Electronics/Security Tech
+    6,                          # First Aid
+    0,                          # Forgery
+    0,                          # Paramedic
+    0,                          # Photography/Film
+    0,                          # Pick Lock
+    0,                          # Pick Pocket
+    6),                         # Play Instrument
 
-    [ 5,                 # TECH
-      0,                 # Electronics/Security Tech
-      6,                 # First Aid
-      0,                 # Forgery
-      0,                 # Paramedic
-      0,                 # Photography/Film
-      0,                 # Pick Lock
-      0,                 # Pick Pocket
-      6 ],               # Play Instrument
+    (7,                         # COOL
+    0,                          # Acting
+    0,                          # Bribery
+    0,                          # Interrogation
+    6,                          # Persuation
+    6,                          # Streetwise
+    0,                          # Trading
+    4),                         # Wardrobe & Style
 
-    [ 7,                 # COOL
-      0,                 # Acting
-      0,                 # Bribery
-      0,                 # Interrogation
-      6,                 # Persuation
-      6,                 # Streetwise
-      0,                 # Trading
-      4 ],               # Wardrobe & Style
+    (8,                         # WILL
+    2,                          # Concentration
+    0),                         # Resist Torture/Drugs
 
-    [ 8,                 # WILL
-      2,                 # Concentration
-      0 ],               # Resist Torture/Drugs
+    5,                          # LUCK
+    7,                          # MOVE
+    3,                          # BODY
 
-      5,                 # LUCK
-      7,                 # MOVE
-      3,                 # BODY
+    (6,                         # EMP
+    2,                          # Conversation
+    6),                         # Human Perception
 
-    [ 6,                 # EMP
-      2,                 # Conversation
-      6 ],               # Human Perception
+    40,                         # HP
+    20,                         # seriously_wounded
+    3,                          # death_save
 
-      40,                # HP
-      20,                # seriously_wounded
-      3,                 # death_save
+    ("Light Armorjack", 11),    # Armor, SP
 
-    [ "Light Armorjack", # Armor
-      11 ],                # SP
+    ("Very Heavy Pistol",       # Weapon 1
+    4,                          # DMG 4d6
+    8,                          # Ammo
+    1,                          # ROF
+    16),                        # extra ammo 
+    
+    ("Heavy Melee Weapon",      # Weapon 2
+    3,                          # DMG 3d6
+    None,                       # Ammo
+    2,                          # ROF
+    ("Sword", "Baseball Bat")), # 1 Melee Weapon
+    ("Charismatic Impact", 2),  # Role Ability, EMP/COOL 
+    "Internal Agent",           # Cyberware 1
+    "Paint Editor Chipware",    # Cyberware 2
+    (("Guitar", "Synthesizer", 
+    "Sampler", "Sequencer"),
+    "Pocket Amp", "Glow Paint",
+    "Video Camera"),            # Gear
+    "Ascii Portrait",           # Portrait 
+    "Notes"                     # Notes
+    ),
 
-    [ "Very Heavy Pistol", # Weapon 1
-      "4d6",                # DMG
-      8,                # Ammo
-      1,                # ROF
-      "You have 16 rounds of extra ammunition" ], # Notes
-    [ "Heavy Melee Weapon",                       # Weapon 2
-      "3d6",                                      # DMG
-      None,                                       # Ammo
-      2,                                          # ROF
-      "A sword or a baseball bat. Your choice." ], # Notes
-    """Charismatic Impact
-    You know when someone is a fan and            
-    receive a +2 to any EMP or COOL 
-    based Skill Check made against them,
-    including Facedowns.""",                         # Role Ability
-    """Internal Agent
-    You have a self-adaptive AI-powered Smart Phone in your head,
-    controlled entirely by voice command.""",         # Cyberware 1
-    """Paint Editor Chipware
-    You can shut off your pain receptors, ignoring you to ignore the -2 to all
-    Checks granted by the Seriously Wounded Wound State.""", # Cyberware 2
-    {"Musical Instrument": "Player's choice",
-    "Pocket Amp": "Amplifies musical instrument",
-    "Glow Paint": "Glow in the dark spraypaint",
-    "Video Camera": "Records up to 12 hours"}, # Gear
-    "ascii representation of character",        # Portrait 
-    "Notes"                                    # Notes
-    ],
+    "Mover": (                  # Handle  
+    "Solo",                     # Role
 
-    "Forty": [             # Handle  
-    "Rockerboy",         # Role
+    (7,                         # INT
+    0,                          # Accounting
+    0,                          # Bureaucracy
+    0,                          # Business
+    0,                          # Composition
+    8,                          # Conceal/Reveal Object
+    0,                          # Criminology
+    0,                          # Cryptography
+    0,                          # Deduction
+    2,                          # Education
+    0,                          # Library Search
+    2,                          # Local Expert
+    8,                          # Perception
+    6,                          # Tactics
+    0),                         # Tracking
 
-    [ 5,                 # INT
-      0,                 # Accounting
-      0,                 # Bureaucracy
-      0,                 # Business
-      6,                 # Composition
-      0,                 # Conceal/Reveal Object
-      0,                 # Criminology
-      0,                 # Cryptography
-      0,                 # Deduction
-      2,                 # Education
-      0,                 # Library Search
-      4,                 # Local Expert
-      2,                 # Perception
-      0,                 # Tactics
-      0 ],               # Tracking
+    (7,                         # REF
+    0,                          # Drive Land Vehicle
+    6,                          # Handgun
+    6),                         # Shoulder Arms
 
-    [ 6,                 # REF
-      0,                 # Drive Land Vehicle
-      6,                 # Handgun
-      0 ],               # Shoulder Arms
+    (6,                         # DEX
+    2,                          # Athletics
+    (2, 3)                      # Brawling, 3d6
+    6,                          # Evasion
+    0,                          # Melee Weapon
+    6),                         # Stealth
 
-    [ 7,                 # DEX
-      2,                 # Athletics
-      6,                 # Brawling
-      6,                 # Evasion
-      6,                 # Melee Weapon
-      2 ],               # Stealth
+    (5,                         # TECH
+    0,                          # Electronics/Security Tech
+    6,                          # First Aid
+    0,                          # Forgery
+    0,                          # Paramedic
+    0,                          # Photography/Film
+    0,                          # Pick Lock
+    0,                          # Pick Pocket
+    0),                         # Play Instrument
 
-    [ 5,                 # TECH
-      0,                 # Electronics/Security Tech
-      6,                 # First Aid
-      0,                 # Forgery
-      0,                 # Paramedic
-      0,                 # Photography/Film
-      0,                 # Pick Lock
-      0,                 # Pick Pocket
-      6 ],               # Play Instrument
+    (7,                         # COOL
+    0,                          # Acting
+    0,                          # Bribery
+    6,                          # Interrogation
+    2,                          # Persuation
+    0,                          # Streetwise
+    0,                          # Trading
+    0),                         # Wardrobe & Style
 
-    [ 7,                 # COOL
-      0,                 # Acting
-      0,                 # Bribery
-      0,                 # Interrogation
-      6,                 # Persuation
-      6,                 # Streetwise
-      0,                 # Trading
-      4 ],               # Wardrobe & Style
+    (6,                         # WILL
+    2,                          # Concentration
+    6),                         # Resist Torture/Drugs
 
-    [ 8,                 # WILL
-      2,                 # Concentration
-      0 ],               # Resist Torture/Drugs
+    6,                          # LUCK
+    7,                          # MOVE
+    7,                          # BODY
 
-      5,                 # LUCK
-      7,                 # MOVE
-      3,                 # BODY
+    (3,                         # EMP
+    2,                          # Conversation
+    2),                         # Human Perception
 
-    [ 6,                 # EMP
-      2,                 # Conversation
-      6 ],               # Human Perception
+    45,                         # HP
+    23,                         # seriously_wounded
+    7,                          # death_save
 
-      40,                # HP
-      20,                # seriously_wounded
-      3,                 # death_save
-
-    [ "Light Armorjack", # Armor
-      11 ],                # SP
-
-    [ "Very Heavy Pistol", # Weapon 1
-      "4d6",                # DMG
-      8,                # Ammo
-      1,                # ROF
-      "You have 16 rounds of extra ammunition" ], # Notes
-    [ "Heavy Melee Weapon",                       # Weapon 2
-      "3d6",                                      # DMG
-      None,                                       # Ammo
-      2,                                          # ROF
-      "A sword or a baseball bat. Your choice." ], # Notes
-    """Charismatic Impact
-    You know when someone is a fan and            
-    receive a +2 to any EMP or COOL 
-    based Skill Check made against them,
-    including Facedowns.""",                         # Role Ability
-    """Internal Agent
-    You have a self-adaptive AI-powered Smart Phone in your head,
-    controlled entirely by voice command.""",         # Cyberware 1
-    """Paint Editor Chipware
-    You can shut off your pain receptors, ignoring you to ignore the -2 to all
-    Checks granted by the Seriously Wounded Wound State.""", # Cyberware 2
-    {"Musical Instrument": "Player's choice",
-    "Pocket Amp": "Amplifies musical instrument",
-    "Glow Paint": "Glow in the dark spraypaint",
-    "Video Camera": "Records up to 12 hours"}, # Gear
-    "ascii representation of character",        # Portrait 
-    "Notes" ]                                  # Notes
+    ("Light Armorjack", 11),    # Armor, SP
+    ("Shotgun",                 # Weapon 1
+    5,                          # DMG 5d6
+    4,                          # Ammo
+    1,                          # ROF
+    8),                         # extra ammo
+    ("Assault Rifle",           # Weapon 2
+    5,                          # DMG 5d6
+    25,                         # Ammo
+    1,                          # ROF
+    25),                        # extra ammo
+    "Combat Awareness",         # Role Ability
+    "Image Enhance Cybereyes",  # Cyberware 1
+    "Teleoptic Cybereye",       # Cyberware 2
+    "Burner Phone",             # Gear
+    "Ascii Portrait",           # Portrait 
+    "Notes")                                  # Notes
 }
-
