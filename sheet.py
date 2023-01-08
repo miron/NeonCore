5a2159710db285c1ce8019b6546b8e4714c89e58
Miron <miron@users.noreply.github.com>
2023-01-08 08:38:55 +0100

    solo stats

--- sheet.py
+++ sheet.py
@@ -20,46 +20,46 @@ characters = [
             "death_save": 3
         }, 
         "skills": {
-            "accounting": [0, 0],
-            "acting": [0, 0],
+            "accounting": [5, 0],
+            "acting": [7, 0],
             "athletics": [7, 2],
             "brawling": [7, 6, 1],
-            "bribery": [0, 0],
-            "bureaucracy": [0, 0],
-            "business": [0, 0],
+            "bribery": [7, 0],
+            "bureaucracy": [5, 0],
+            "business": [5, 0],
             "composition": [5, 6],
-            "conceal_reveal_object": [0, 0],
+            "conceal_reveal_object": [5, 0],
             "concentration": [8, 2],
             "conversation": [6, 2],
-            "criminology": [0, 0],
-            "cryptography": [0, 0],
-            "deduction": [0, 0],
-            "drive_land_vehicle": (0, 0),
+            "criminology": [5, 0],
+            "cryptography": [5, 0],
+            "deduction": [5, 0],
+            "drive_land_vehicle": (6, 0),
             "education": [5, 2],
-            "electronics_security_tech": [0, 0],
+            "electronics_security_tech": [5, 0],
             "evasion": [7, 6],
             "first_aid": [5, 6],
-            "forgery": [0, 0],
+            "forgery": [5, 0],
             "handgun": [6, 6],
             "human_perception": [6, 6],
-            "interrogation": [0, 0],
-            "library_search": [0, 0],
+            "interrogation": [7, 0],
+            "library_search": [5, 0],
             "local_expert": [5, 4],
             "melee_weapon": [7, 6],
-            "paramedic": [0, 0],
+            "paramedic": [5, 0],
             "perception": [5, 2],
             "persuation": [7, 6],
-            "photography_film": [0, 0],
-            "pick_lock": [0, 0],
-            "pick_pocket": [0, 0],
+            "photography_film": [5, 0],
+            "pick_lock": [5, 0],
+            "pick_pocket": [5, 0],
             "play_instrument": [5, 6],
-            "resist_torture_drugs": [0, 0],
-            "shoulder_arms": [0, 0],
+            "resist_torture_drugs": [8, 0],
+            "shoulder_arms": [6, 0],
             "stealth": [7, 2],
             "streetwise": [7, 6],
-            "tactics": [0, 0],
-            "tracking": [0, 0],
-            "trading": [0, 0],
+            "tactics": [5, 0],
+            "tracking": [5, 0],
+            "trading": [7, 0],
             "wardrobe_and_style": [7, 4]
         },
         "armor": {
@@ -124,77 +124,129 @@ characters = [
                      >^<
                      """
     },
-    # Add more character dictionaries here
+    {
+        "handle": "Mover",
+        "role": "Solo",
+        "stats": {
+            "int": 7,
+            "ref": 7,
+            "dex": 6,
+            "tech": 5,
+            "cool": 7,
+            "will": 6,
+            "luck": 6,
+            "move": 7,
+            "body": 7,
+            "emp": 3
+        },
+        "combat": {
+            "hp": 15,
+            "seriously_wounded": 10,
+            "death_save": 3
+        }, 
+        "skills": {
+            "accounting": [5, 0],
+            "acting": [7, 0],
+            "athletics": [7, 2],
+            "brawling": [7, 6, 1],
+            "bribery": [7, 0],
+            "bureaucracy": [5, 0],
+            "business": [5, 0],
+            "composition": [5, 6],
+            "conceal_reveal_object": [5, 0],
+            "concentration": [8, 2],
+            "conversation": [6, 2],
+            "criminology": [5, 0],
+            "cryptography": [5, 0],
+            "deduction": [5, 0],
+            "drive_land_vehicle": (6, 0),
+            "education": [5, 2],
+            "electronics_security_tech": [5, 0],
+            "evasion": [7, 6],
+            "first_aid": [5, 6],
+            "forgery": [5, 0],
+            "handgun": [6, 6],
+            "human_perception": [6, 6],
+            "interrogation": [7, 0],
+            "library_search": [5, 0],
+            "local_expert": [5, 4],
+            "melee_weapon": [7, 6],
+            "paramedic": [5, 0],
+            "perception": [5, 2],
+            "persuation": [7, 6],
+            "photography_film": [5, 0],
+            "pick_lock": [5, 0],
+            "pick_pocket": [5, 0],
+            "play_instrument": [5, 6],
+            "resist_torture_drugs": [8, 0],
+            "shoulder_arms": [6, 0],
+            "stealth": [7, 2],
+            "streetwise": [7, 6],
+            "tactics": [5, 0],
+            "tracking": [5, 0],
+            "trading": [7, 0],
+            "wardrobe_and_style": [7, 4]
+        },
+        "armor": {
+            "name": "Light Armorjack",
+            "sp": 11
+        },
+        "weapon": [
+            {
+                "name": "Very Heavy Pistol",
+                "dmg": 4,
+                "ammo": 8,
+                "rof": 1,
+                "notes": 16
+            },
+            {   "name": "Heavy Melee Weapon",
+                "dmg": 3,
+                "ammo": None,
+                "rof": 2
+                "notes": "sword/basball bat"
+            }, 
+        ],
+        "role_ability": {
+            "name": "Charismatic Impact",
+            "notes": """You know when someone is a fan and
+            receive a +2 to any EMP or COOL
+            based Skill Check made against them,
+            including Facedowns."""
+        },
+        "cyberware": [
+            {
+                "name": "Internal Agent",
+                "notes": """You have a self-adaptive AI-powered Smart Phone in your head, 
+                 controlled entirely by voice command"""
+            },
+            {  
+                "name": "Pain Editor Chipware",
+                "notes": """You can shut off your pain receptors, ignoring you to ignore the -2 to all
+                 Checks granted by the Seriously Wounded Wound State."""
+            }
+        ],
+        "gear": [
+            {   
+                "name": "Musical Instrument",
+                "note": "Player's choice"
+            },
+            {
+                "name": "Pocket Amp",
+                "note": "Amplifies musical instrument"
+            },
+            {
+                "name": "Glow Paint",
+                "note": "Glow in the dark spraypaint"
+            },
+            {
+                "name": "Video Camera",
+                "note": "Records up to 12 hours"
+            }
+        ],
+        "ascii_art": """
+                    /\_/\
+                   ( o.o )
+                     >^<
+                     """
+    },
 ]
-
-    ("Mover", "Solo",
-    (7, 0, 0, 0, 0, 8, 0, 0, 0, 2, 0, 2, 8, 6, 0),
-    (7, 0, 6, 6),
-    (6, 2, (2, 3), 6, 0, 6),
-    (5, 0, 6, 0, 0, 0, 0, 0, 0),                         
-    (7, 0, 0, 6, 2, 0, 0, 0),
-    (6, 2, 6),
-    6, 7, 7,
-    (3, 2, 2),
-    45, 23, 7,
-    ("Light Armorjack", 11),
-    ("Shotgun", 5, 4, 1, 8),
-    ("Assault Rifle", 5, 25, 1, 25),
-    "Combat Awareness",
-    "Image Enhance Cybereyes", "Teleoptic Cybereye",
-    "Burner Phone",
-    "Ascii Portrait", "Notes"),
-    
-    ("Torch", "Tech",
-    (8, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 2, 2, 0, 0),
-    (7, 0, 6, 6),
-    (5, 2, (2, 2), 6, 0, 6),
-    (6, 10, 6, 6, 0, 0, 6, 6, 0),
-    (3, 0, 0, 0, 2, 0, 0, 0),
-    (3, 2, 0),
-    7, 6, 6,
-    (5, 2, 2),
-    35, 18, 6,
-    ("Light Armorjack", 11),
-    ("Heavy Pistol", 3, 8, 2, 16),
-    ("Shotgun", 5, 4, 1, 8),
-    "Maker",
-    "Tool Hand", "Internal Agent",
-    ("Duct Tape", "Flashlight", "Road Flare"),
-    "ASCII Portrait", "Notes"),
-    
-    ("Redtail", "Medtech",
-    (8, 0, 0, 0, 0, 0, 0, 0, 6, 6, 0, 2, 6, 0, 0),
-    (5, 0, 0, 6),
-    (5, 2, (2, 2), 6, 0, 6),
-    (8, 4, 2, 0, 6, 0, 0, 0, 0),
-    (5, 0, 0, 0, 5, 0, 0, 0),
-    (5, 2, 4),
-    6, 6, 5,
-    (4, 6, 6),
-    35, 18, 5,
-    ("Light Armorjack", 11),
-    ("Shotgun", 5, 4, 1, 8),
-    ("Light Melee Weapon", 1, None, 2, "Small Knife"),
-    "Medicine",
-    "Tool Hand", "Pain Editor Chipware",
-    ("Agent", "Medtech Bag", "Glow Paint", "Flashlight"),
-    "ASCII Portrait", "Notes"),
-    
-    ("24/7", "Media",
-    (7, 0, 0, 0, 6, 8, 0, 0, 0, 2, 4, 6, 8, 0, 0),
-    (5, 0, 6, 0),
-    (5, 2, (2, 2), 6, 6, 6),
-    (4, 0, 2, 6, 0, 0, 0, 0, 0),
-    (8, 0, 0, 0, 6, 0, 0, 0),
-    (7, 2, 0),
-    6, 7, 5,
-    (7, 6, 6),
-    40, 20, 5,
-    ("Light Armorjack", 11),
-    ("Heavy Pistol", 3, 8, 2, 16),
-    ("Heavy Melee Weapon", 3, None, 2, ("Sword", "Folded Tripod")),
-    "Credibility",
-    "Internal Agent", "Image Enhance Cybereyes",
-    ("Video Camera", "Audio Recorder"),
-    "ASCII Portrait", "Notes"))
