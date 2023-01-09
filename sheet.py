14ca6ee83caeeb48dce0f4649c5b74ed7089b657
Miron <miron@users.noreply.github.com>
2023-01-10 00:54:05 +0100

    finished player character config

--- sheet.py
+++ sheet.py
@@ -86,18 +86,21 @@ characters = [
             "notes": """You know when someone is a fan and
             receive a +2 to any EMP or COOL
             based Skill Check made against them,
-            including Facedowns."""
+            including Facedowns.
+            """
         },
         "cyberware": [
             {
                 "name": "Internal Agent",
                 "notes": """You have a self-adaptive AI-powered Smart Phone in your head, 
-                 controlled entirely by voice command"""
+                 controlled entirely by voice command.
+                 """
             },
             {  
                 "name": "Pain Editor Chipware",
                 "notes": """You can shut off your pain receptors, ignoring you to ignore the -2 to all
-                 Checks granted by the Seriously Wounded Wound State."""
+                 Checks granted by the Seriously Wounded Wound State.
+                 """
             }
         ],
         "gear": [
@@ -125,7 +128,8 @@ characters = [
             Y¸∆
           ¸⁄∫\ 
            / |   
-          ∫  l """
+          ∫  l
+          """
     },
     {
         "handle": "Mover",
@@ -217,7 +221,8 @@ characters = [
             {
                 "name": "Image Enhance Cybereyes",
                 "notes": """Your eyes are better than human, giving you +2 to Perception and
-                 Conceal/Reveal Object Skills (already included above)."""
+                 Conceal/Reveal Object Skills (already included above).
+                 """
             },
             {  
                 "name": "Teleoptic Cybereye",
@@ -237,7 +242,8 @@ characters = [
         \ᡕᠵ᠊╦╤÷─
          7˘ \
          |  ]
-         l  L"""
+         l  L
+         """
     },
     {
         "handle": "Torch",
@@ -297,7 +303,7 @@ characters = [
             "shoulder_arms": [7, 6],
             "stealth": [5, 6],
             "streetwise": [3, 0],
-            "tactics": [0, 0],
+            "tactics": [8, 0],
             "tracking": [8, 0],
             "trading": [3, 0],
             "wardrobe_and_style": [3, 0]
@@ -324,18 +330,21 @@ characters = [
         "role_ability": {
             "name": "Maker",
             "notes": """Your Electronics/Security Tech Skill
-            is boosted by +4 (already included above)."""
+            is boosted by +4 (already included above).
+            """
         },
         "cyberware": [
             {
                 "name": "Tool Hand",
                 "notes": """Your fingers contain a screwdriver, wrench, small drill, and any other
-                tool you might need in the field."""
+                tool you might need in the field.
+                """
             },
             {  
                 "name": "Internal Agent",
                 "notes": """You have a self-adaptive AI-powered Smart Phone in your head,
-                controlled entirely by voice command."""
+                controlled entirely by voice command.
+                """
             }
         ],
         "gear": [
@@ -358,4 +367,243 @@ characters = [
                      >^<
                      """
     },
+    {
+        "handle": "Redtail",
+        "role": "Medtech",
+        "stats": {
+            "int": 8,
+            "ref": 5,
+            "dex": 5,
+            "tech": 8,
+            "cool": 5,
+            "will": 5,
+            "luck": 6,
+            "move": 6,
+            "body": 5,
+            "emp": 4
+        },
+        "combat": {
+            "hp": 35,
+            "seriously_wounded": 18,
+            "death_save": 5
+        }, 
+        "skills": {
+            "accounting": [8, 0],
+            "acting": [5, 0],
+            "athletics": [5, 2],
+            "brawling": [5, 2, 2],
+            "bribery": [5, 0],
+            "bureaucracy": [8, 0],
+            "business": [8, 0],
+            "composition": [8, 0],
+            "conceal_reveal_object": [8, 0],
+            "concentration": [5, 2],
+            "conversation": [4, 6],
+            "criminology": [8, 0],
+            "cryptography": [8, 0],
+            "education": [8, 6],
+            "electronics_security_tech": [8, 4],
+            "evasion": [5, 6],
+            "first_aid": [8, 2],
+            "forgery": [8, 0],
+            "handgun": [5, 0],
+            "human_perception": [4, 6],
+            "interrogation": [5, 0],
+            "library_search": [8, 0],
+            "local_expert": [8, 2],
+            "melee_weapon": [5, 0],
+            "paramedic": [8, 6],
+            "perception": [8, 6],
+            "persuation": [5, 5],
+            "photography_film": [8, 0],
+            "pick_lock": [8, 0],
+            "pick_pocket": [8, 0],
+            "play_instrument": [8, 0],
+            "resist_torture_drugs": [5, 4],
+            "shoulder_arms": [5, 6],
+            "stealth": [5, 6],
+            "streetwise": [5, 0],
+            "surgery": [8, 4],
+            "tactics": [8, 0],
+            "tracking": [8, 0],
+            "trading": [5, 0],
+            "wardrobe_and_style": [5, 0]
+        },
+        "armor": {
+            "name": "Light Armorjack",
+            "sp": 11
+        },
+        "weapon": [
+            {
+                "name": "Shotgun",
+                "dmg": 5,
+                "ammo": 4,
+                "rof": 1,
+                "notes": 8
+            },
+            {   "name": "Light Melee Weapon",
+                "dmg": 1,
+                "ammo": None,
+                "rof": 2,
+                "notes": "small knife"
+            }, 
+        ],
+        "role_ability": {
+            "name": "Medicine",
+            "notes": """You have access to the Surgery Skill
+            (already included above).
+            """
+        },
+        "cyberware": [
+            {
+                "name": "Tool Hand",
+                "notes": """Your fingers contain a screwdriver, wrench, small drill, and any other
+                tool you might neew in the field.
+                """
+            },
+            {  
+                "name": "Pain Editor Chipware",
+                "notes": """You can shut off your pain receptors, ignoring you to ignore the -2 to all
+                 Checks granted by the Seriously Wounded Wound State."""
+            }
+        ],
+        "gear": [
+            {   
+                "name": "Agent",
+                "note": "AI-powered smartphone"
+            },
+            {
+                "name": "Medtech Bag",
+                "note": "Full selection of medical tools"
+            },
+            {
+                "name": "Glow Paint",
+                "note": "Glow in the dark spraypaint"
+            },
+            {
+                "name": "Flashlight",
+                "note": "100m/yd long beam of light"
+            }
+        ],
+        "ascii_art": """
+        """
+    },
+    {
+        "handle": "24/7",
+        "role": "Media",
+        "stats": {
+            "int": 7,
+            "ref": 5,
+            "dex": 5,
+            "tech": 4,
+            "cool": 8,
+            "will": 7,
+            "luck": 6,
+            "move": 7,
+            "body": 5,
+            "emp": 7
+        },
+        "combat": {
+            "hp": 40,
+            "seriously_wounded": 20,
+            "death_save": 5
+        }, 
+        "skills": {
+            "accounting": [7, 0],
+            "acting": [8, 0],
+            "athletics": [5, 2],
+            "brawling": [5 2, 2],
+            "bribery": [8, 0],
+            "bureaucracy": [7, 0],
+            "business": [7, 0],
+            "composition": [8, 6],
+            "conceal_reveal_object": [7, 8],
+            "concentration": [7, 2],
+            "conversation": [7 6],
+            "criminology": [7, 0],
+            "cryptography": [7, 0],
+            "deduction": [7, 0],
+            "drive_land_vehicle": (5, 0),
+            "education": [7, 2],
+            "electronics_security_tech": [4, 0],
+            "evasion": [5, 6],
+            "first_aid": [4, 2],
+            "forgery": [4, 6],
+            "handgun": [5, 6],
+            "human_perception": [7, 6],
+            "interrogation": [8, 0],
+            "library_search": [7, 4],
+            "local_expert": [7, 6],
+            "melee_weapon": [5, 6],
+            "paramedic": [4, 0],
+            "perception": [7, 8],
+            "persuation": [8, 6],
+            "photography_film": [4, 0],
+            "pick_lock": [4, 0],
+            "pick_pocket": [4, 0],
+            "play_instrument": [4, 0],
+            "resist_torture_drugs": [7, 0],
+            "shoulder_arms": [5, 0],
+            "stealth": [5, 6],
+            "streetwise": [8, 0],
+            "tactics": [7, 0],
+            "tracking": [7, 0],
+            "trading": [8, 0],
+            "wardrobe_and_style": [8, 0]
+        },
+        "armor": {
+            "name": "Light Armorjack",
+            "sp": 11
+        },
+        "weapon": [
+            {
+                "name": "Heavy Pistol",
+                "dmg": 3,
+                "ammo": 8,
+                "rof": 2,
+                "notes": 16
+            },
+            {   "name": "Heavy Melee Weapon",
+                "dmg": 3,
+                "ammo": None,
+                "rof": 2,
+                "notes": "sword/folded tripod"
+            }, 
+        ],
+        "role_ability": {
+            "name": "Credibility",
+            "notes": """Once per hour, you may roll a 1d10.
+            If you roll higher than 4, you learn a
+            rumor pertinent to your current 
+            situation. 
+            """
+        },
+        "cyberware": [
+            {
+                "name": "Internal Agent",
+                "notes": """You have a self-adaptive AI-powered Smart Phone in your head, 
+                 controlled entirely by voice command.
+                 """
+            },
+            {  
+                "name": "Image Enhance Cybereyes",
+                "notes": """Your eyes are better than human, giving yu a +2 to Perception and
+                Conceal/Reveal Object Skills (already included above).
+                """
+            }
+        ],
+        "gear": [
+            {   
+                "name": "Video Camera",
+                "note": "Records up to 12 hours"
+            },
+            {
+                "name": "Audio Recorder",
+                "note": "Records up to 24 hours"
+            }
+                "note": "Records up to 12 hours"
+        ],
+        "ascii_art": """
+        """
+    }
 ]
