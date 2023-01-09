16a0cefe7f621bf4651e5a996b4bc4897132ddea
Miron <miron@users.noreply.github.com>
2023-01-09 18:28:40 +0100

    added Tech data

--- sheet.py
+++ sheet.py
@@ -233,67 +233,67 @@ characters = [
         "ascii_art": "︻デ┳═ー"
     },
     {
-        "handle": "Forty",
-        "role": "Rockerboy",
+        "handle": "Torch",
+        "role": "Tech",
         "stats": {
-            "int": 5,
-            "ref": 6,
-            "dex": 7,
-            "tech": 5,
-            "cool": 7,
-            "will": 8,
-            "luck": 5,
-            "move": 7,
-            "body": 3,
-            "emp": 6
+            "int": 8,
+            "ref": 7,
+            "dex": 5,
+            "tech": 6,
+            "cool": 3,
+            "will": 3,
+            "luck": 7,
+            "move": 6,
+            "body": 6,
+            "emp": 5
         },
         "combat": {
-            "hp": 15,
-            "seriously_wounded": 10,
-            "death_save": 3
+            "hp": 35,
+            "seriously_wounded": 18,
+            "death_save": 6
         }, 
         "skills": {
-            "accounting": [5, 0],
-            "acting": [7, 0],
-            "athletics": [7, 2],
-            "brawling": [7, 6, 1],
-            "bribery": [7, 0],
-            "bureaucracy": [5, 0],
-            "business": [5, 0],
-            "composition": [5, 6],
-            "conceal_reveal_object": [5, 0],
-            "concentration": [8, 2],
-            "conversation": [6, 2],
-            "criminology": [5, 0],
-            "cryptography": [5, 0],
-            "deduction": [5, 0],
-            "drive_land_vehicle": (6, 0),
-            "education": [5, 2],
-            "electronics_security_tech": [5, 0],
-            "evasion": [7, 6],
-            "first_aid": [5, 6],
-            "forgery": [5, 0],
-            "handgun": [6, 6],
-            "human_perception": [6, 6],
-            "interrogation": [7, 0],
-            "library_search": [5, 0],
-            "local_expert": [5, 4],
-            "melee_weapon": [7, 6],
-            "paramedic": [5, 0],
-            "perception": [5, 2],
-            "persuation": [7, 6],
-            "photography_film": [5, 0],
-            "pick_lock": [5, 0],
-            "pick_pocket": [5, 0],
-            "play_instrument": [5, 6],
-            "resist_torture_drugs": [8, 0],
-            "shoulder_arms": [6, 0],
-            "stealth": [7, 2],
-            "streetwise": [7, 6],
-            "tactics": [5, 0],
-            "tracking": [5, 0],
-            "trading": [7, 0],
-            "wardrobe_and_style": [7, 4]
+            "accounting": [8, 0],
+            "acting": [3, 0],
+            "athletics": [5, 2],
+            "brawling": [5, 2, 2],
+            "bribery": [3, 0],
+            "bureaucracy": [8, 0],
+            "business": [8, 0],
+            "composition": [8, 0],
+            "conceal_reveal_object": [8, 0],
+            "concentration": [3, 2],
+            "conversation": [5, 2],
+            "criminology": [8, 0],
+            "cryptography": [8, 0],
+            "deduction": [8, 0],
+            "drive_land_vehicle": (7, 0),
+            "education": [8, 6],
+            "electronics_security_tech": [6, 10],
+            "evasion": [5, 6],
+            "first_aid": [6, 6],
+            "forgery": [6, 6],
+            "handgun": [7, 6],
+            "human_perception": [5, 2],
+            "interrogation": [3, 0],
+            "library_search": [8, 0],
+            "local_expert": [8, 2],
+            "melee_weapon": [5, 0],
+            "paramedic": [6, 0],
+            "perception": [8, 2],
+            "persuation": [3, 2],
+            "photography_film": [6, 0],
+            "pick_lock": [6, 6],
+            "pick_pocket": [6, 6],
+            "play_instrument": [6, 0],
+            "resist_torture_drugs": [3, 0],
+            "shoulder_arms": [7, 6],
+            "stealth": [5, 6],
+            "streetwise": [3, 0],
+            "tactics": [0, 0],
+            "tracking": [8, 0],
+            "trading": [3, 0],
+            "wardrobe_and_style": [3, 0]
         },
         "armor": {
             "name": "Light Armorjack",
@@ -301,54 +301,48 @@ characters = [
         },
         "weapon": [
             {
-                "name": "Very Heavy Pistol",
-                "dmg": 4,
+                "name": "Heavy Pistol",
+                "dmg": 3,
                 "ammo": 8,
-                "rof": 1,
+                "rof": 2,
                 "notes": 16
             },
-            {   "name": "Heavy Melee Weapon",
-                "dmg": 3,
-                "ammo": None,
-                "rof": 2,
-                "notes": "sword/basball bat"
+            {   "name": "Shotgun",
+                "dmg": 5,
+                "ammo": "4",
+                "rof": 1,
+                "notes": 8 
             }, 
         ],
         "role_ability": {
-            "name": "Charismatic Impact",
-            "notes": """You know when someone is a fan and
-            receive a +2 to any EMP or COOL
-            based Skill Check made against them,
-            including Facedowns."""
+            "name": "Maker",
+            "notes": """Your Electronics/Security Tech Skill
+            is boosted by +4 (already included above)."""
         },
         "cyberware": [
             {
-                "name": "Internal Agent",
-                "notes": """You have a self-adaptive AI-powered Smart Phone in your head, 
-                 controlled entirely by voice command"""
+                "name": "Tool Hand",
+                "notes": """Your fingers contain a screwdriver, wrench, small drill, and any other
+                tool you might need in the field."""
             },
             {  
-                "name": "Pain Editor Chipware",
-                "notes": """You can shut off your pain receptors, ignoring you to ignore the -2 to all
-                 Checks granted by the Seriously Wounded Wound State."""
+                "name": "Internal Agent",
+                "notes": """You have a self-adaptive AI-powered Smart Phone in your head,
+                controlled entirely by voice command."""
             }
         ],
         "gear": [
             {   
-                "name": "Musical Instrument",
-                "note": "Player's choice"
+                "name": "Duct Tape",
+                "note": "Perfect for fixing anything"
             },
             {
-                "name": "Pocket Amp",
-                "note": "Amplifies musical instrument"
+                "name": "Flashlight",
+                "note": "100m/yd long bean of light"
             },
             {
-                "name": "Glow Paint",
-                "note": "Glow in the dark spraypaint"
-            },
-            {
-                "name": "Video Camera",
-                "note": "Records up to 12 hours"
+                "name": "Road Flare",
+                "note": "Lights up a 100m/yd area"
             }
         ],
         "ascii_art": """
