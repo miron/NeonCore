ee9a9f436cf55d553920b3ff185bab1149f29621
root <m.iron@icloud.com>
2023-01-18 17:19:13 +0000

    some PEP8 ->boosting productivity, more loc

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 """A Role Playing Game in the Cyberpunk RED Universe"""
+
 import cmd
 import os
 import sys
@@ -21,7 +22,7 @@ DIFFICULTY_VALUE = {
     "Professional": 17,
     "Heroic": 21,
     "Incredible": 24
-}
+    }
 
 class ActionManager(cmd.Cmd):
     """cli, displays character stats/skills, quits the game"""
@@ -49,7 +50,6 @@ class ActionManager(cmd.Cmd):
 
     def do_shell(self, arg):
         """ Shell commands can be added here prefixed with !"""
-        pass
         os.system('clear')
 
    # def postcmd(self, stop, line):
@@ -68,13 +68,13 @@ class ActionManager(cmd.Cmd):
    #     self.stdscr.refresh()
    #     return cmd.Cmd.postcmd(self, stop, line)
 
-
     def default(self, line):
         print("WTF dat mean, ain't no command like dat")
 
     def do_quit(self, arg):
         """Exits Cyberpunk RED"""
-        wprint("Catch you on the flip side, choombatta. Keep your chrome polished and your guns loaded, "
+        wprint("Catch you on the flip side, choombatta. Keep your chrome "
+               "polished and your guns loaded, "
                "the neon jungle ain't no walk in the park.")
         # Open database, create if it doesn't already exist
         with shelve.open('timestamp') as dbase:
@@ -83,11 +83,13 @@ class ActionManager(cmd.Cmd):
         sys.exit()
 
     def do_choose_character(self, arg):
-        """
-        Allows the player to choose a character role.
-        """
-        self.prompt = "Pick yo' ride, rockerboy, solo, tech, medtech, or media:"
-        characters_list = [f"{character.handle} ({character.role})" for i, character in enumerate(self.characters_list)]
+        """Allows the player to choose a character role."""
+        self.prompt = ("Pick yo' ride, rockerboy, solo, tech, medtech, "
+                       "or media:")
+        characters_list = [
+            f"{character.handle} ({character.role})" 
+            for i, character in enumerate(self.characters_list)
+            ]
         self.columnize(characters_list, displaywidth=80)
         valid_roles = ['rockerboy', 'solo', 'tech', 'medtech', 'media']
         if arg in valid_roles:
@@ -96,11 +98,13 @@ class ActionManager(cmd.Cmd):
         else:
             print(f"Invalid role. Valid roles: {valid_roles}")
 
-
     def choose_character(self):
         while True:
             print("Yo, roll a homie, chummer:")
-            characters_list = [f"{i+1}. {character.handle}" for i, character in enumerate(self.characters_list)]
+            characters_list = [
+                f"{i+1}. {character.handle}"
+                for i, character in enumerate(self.characters_list)
+                ]
             self.columnize(characters_list, displaywidth=80)
             choice = input("Enter the number of your choice or 'q' to quit: ")
             if choice.lower() == 'q':
@@ -116,8 +120,8 @@ class ActionManager(cmd.Cmd):
                         break
             except ValueError:
                 pass
-            print("Invalid choice. Please choose a number between 1 and", len(characters_list))
-
+            print("Invalid choice. Please choose a number between 1 and",
+                  len(characters_list))
 
     def do_move(self, args):
         """Move player in the specified direction"""
@@ -137,40 +141,55 @@ class ActionManager(cmd.Cmd):
                     return
             except ValueError:
                 pass
-            print("Invalid choice. Please choose a number between 0 and", len(self.npcs)-1)
+            print("Invalid choice. Please choose a number between 0 and",
+                  len(self.npcs)-1)
     
     def do_player_sheet(self, arg):
         """Displays the character sheet"""
-        print(f"HANDLE \033[1;3;35m{self.player.handle:⌁^33} \033[0mROLE \033[1;3;35m{self.player.role:⌁^33}\033[0m")
-        # Display stats
-        stat_list = [(f'{key:⌁<12}{self.player.lucky_pool}/{value}' if key == 'luck' else f'{key:⌁<12}{value:>2}')
-                        for key, value in self.player.stats.items()]
+        print(f"HANDLE \033[1;3;35m{self.player.handle:⌁^33}\033[0m ROLE "
+              f"\033[1;3;35m{self.player.role:⌁^33}\033[0m")
+        stat_list = [(f'{key:⌁<12}{self.player.lucky_pool}/{value}' 
+                      if key == 'luck' else f'{key:⌁<12}{value:>2}')
+                     for key, value in self.player.stats.items()]
         self.columnize(stat_list, displaywidth=80)
-        # Display combat
         combat_list = [(f'{key:⌁<23}{value:>2}')
                         for key, value in self.player.combat.items()]
         self.columnize(combat_list, displaywidth=80)
         skill_keys = list(self.player.skills.keys())
         skill_values = list(self.player.skills.values())
         skill_list = [(f'{key:⌁<30}{value[0]:>2}')
-                        for key, value in zip(skill_keys,skill_values) if value[1!=0]]
+                      for key, value in zip(skill_keys,skill_values)
+                      if value[1!=0]]
         skill_list += self.player.ascii_art.splitlines()
         self.columnize(skill_list, displaywidth=80)
         # Display armor & weapons
-        defence_list = [f"WEAPONS & ARMOR{'⌁'*19:<10} "] +  [' '.join(self.player.defence.keys())] + [
-        ' '.join([str(row) for row in self.player.defence.values()])]
-        weapons_list = [' '.join(self.player.weapons[0].keys())] + [
-        ' '.join([str(val) for val in row.values()]) for row in self.player.weapons]
+        defence_list = [f"WEAPONS & ARMOR{'⌁'*19:<10} "] \
+                     + [' '.join(self.player.defence.keys())] \
+                     + [' '.join([str(row) 
+                         for row in self.player.defence.values()])]
+        weapons_list = [' '.join(self.player.weapons[0].keys())] \
+                     + [' '.join(
+                                 [str(val) for val in row.values()]
+                                 ) 
+                        for row in self.player.weapons]
         for defence, weapon in zip(defence_list, weapons_list):
             print(defence.ljust(35) + weapon.ljust(45))
-        print("ROLE ABILITY " + "⌁" * 14 + " CYBERWARE " + "⌁" * 17 + " GEAR " + "⌁" * 19 )
+        print("ROLE ABILITY " 
+              + "⌁"*14 
+              + " CYBERWARE " 
+              + "⌁"*17 
+              + " GEAR "
+              + "⌁"*19)
         ability_list = list(self.player.role_ability.values())
         ability_list = [row.splitlines() for row in ability_list]
         ability_list = [item for sublist in ability_list for item in sublist]
-        ware_list = [value for row in self.player.cyberware for key, value in row.items()]
+        ware_list = [value for row in self.player.cyberware for key, value in 
+                     row.items()]
         ware_list = [row.splitlines() for row in ware_list]
         ware_list = [item for sublist in ware_list for item in sublist]
-        gear_list = [' '.join(self.player.gear[0].keys())] + [' '.join(row.values()) for row in self.player.gear] + ['']
+        gear_list = [' '.join(self.player.gear[0].keys())] \
+                  + [' '.join(row.values()) for row in self.player.gear] \
+                  + ['']
         for ability, ware, gear in zip(ability_list, ware_list, gear_list):
             #if ability == ability_list[0]:
             #    ability = "\033[1m" + ability + "\033[0m"
@@ -181,10 +200,11 @@ class ActionManager(cmd.Cmd):
             print(ability.ljust(28) + ware.ljust(28) + gear.ljust(24))
 
     def do_rap_sheet(self, arg):
-        """Yo, dis here's rap_sheet, it's gonna show ya all the deetz on ya character's 
-backstory, where they came from, who they know, and what they're all about.
-It's like peepin' into they mind, know what I'm sayin'? Gotta know ya homies 
-before ya start runnin' with em, ya feel me?i
+        """Yo, dis here's rap_sheet, it's gonna show ya all the deetz on
+ya character's backstory, where they came from, who they know, and what
+they're all about.
+It's like peepin' into they mind, know what I'm sayin'? Gotta know ya 
+homies before ya start runnin' with em, ya feel me?
 """
         print("Lifepath:")
         print("Cultural Region:", self.player.cultural_region)
@@ -202,45 +222,49 @@ before ya start runnin' with em, ya feel me?i
         print("Life Goals:", self.player.life_goal)
 
     def do_jack_in(self, args):
-        """Yo, chummer! You wanna make some eddies and climb the ranks? You wanna be a 
-player in Night City? Type 'jack_in' and let's get this show on the road. Gotta 
-choose your character first, make sure you roll 'em up tight and make the right 
-choice. Remember, in Night City, you gotta be quick on your feet and make the 
-right moves, or you'll end up as another memory on the streets. So, you in or 
-what?
+        """Yo, chummer! You wanna make some eddies and climb the ranks? 
+You wanna be a player in Night City? Type 'jack_in' and let's get this 
+show on the road. Gotta choose your character first, make sure you roll 
+'em up tight and make the right choice. Remember, in Night City, you 
+gotta be quick on your feet and make the right moves, or you'll end up 
+as another memory on the streets. So, you in or what?
 """
         self.character = self.choose_character()
-        wprint("Yo, listen up. You and your crew just hit the South Night City docks and now you're chillin' with a "
-              "burner phone call from Lazlo, your fixer.")
+        wprint("Yo, listen up. You and your crew just hit the South Night City"
+               " docks and now you're chillin' with a burner phone call from "
+               "Lazlo, your fixer.")
         wprint("He's all like, 'Yo, we gotta change the spot for the payout. "
-               "Meet me at the industrial park in Heywood.'")
-        wprint("But something ain't right, 'cause Lazlo ain't telling you why. "
-               "He's just saying it's all good, but you can "
-               "tell he's sweatin'.")
+               "Meet me at the industrial park in Heywood.")
+        wprint("But something ain't right, 'cause Lazlo ain't telling you why."
+               " He's just saying it's all good, but you can tell "
+               "he's sweatin'.")
         print("You got a bad feeling about this. Like, real bad.")
         self.game_state = 'before_perception_check'
         self.prompt = "ε(๏_๏)з】 "
 
     def do_perception_check(self, args):
         if args not in ("yes", "no"):
-            wprint("Yo, chummer, you wanna roll for perception check? Type in 'yes' or 'no' to make your choice.")
+            wprint("Yo, chummer, you wanna roll for perception check?"
+                   "Type in 'yes' or 'no' to make your choice.")
             return
-
         if args == "yes":
             roll = random.randint(1, 10)
             human_perception = self.player.skill_total("human_perception")
             if roll + human_perception > 17:
-                wprint("Yo, you're suspecting something's off. You're right, Lazlo's "
-                       "being held at gunpoint and is being forced to lure you into a trap.")
+                wprint("Yo, you're suspecting something's off. You're right, "
+                       "Lazlo's being held at gunpoint and is being forced to "                       "lure you into a trap.")
             else:
-                print("You didn't suspect anything unusual with the phone call.")
+                print(
+                    "You didn't suspect anything unusual with the phone call."
+                    )
         else:
             print("Alright, play it cool.")
         print("Lazlo hangs up before you can ask any more questions.")
         return self.do_heywood_industrial()
 
     def complete_perception_check(self, text, line, begidx, endidx):
-        return ['yes', 'no'] if not text else [c for c in ['yes', 'no'] if c.startswith(text)]
+        return ['yes', 'no'] if not text else [c for c in ['yes', 'no'] 
+                                               if c.startswith(text)]
 
     def completenames(self, text, *ignored):
         cmds = super().completenames(text, *ignored)
@@ -250,21 +274,19 @@ what?
 
     def do_heywood_industrial(self):
         """This method handles the Heywood Industrial story mode."""
-        print("You arrive at Heywood Industrial. How do you want to approach the situation?")
+        wprint("You arrive at Heywood Industrial. "
+               "How do you want to approach the situation?")
         approach = input("Enter your choice: ")
-
         # Check if the player's approach includes a skill check of 17 or higher
         if self.character.skill_check(17):
             print("Your approach leads to a beneficial situation!")
             # Adjudicate the beneficial situation
             self.beneficial_situation()
         else:
-            print("Your approach does not lead to a beneficial situation.")
-
-        print("At the center of some alleys is a hooded man handcuffed to a briefcase.")
-        print("He offers it to you, but fumbles with the key before handing it over.")
+            wprint("Your approach does not lead to a beneficial situation."
+                   "At the center of some alleys is a hooded man handcuffed to"                   "  a briefcase. He offers it to you, but fumbles with the "
+                   "key before handing it over.")
         choice = input("Do you take the briefcase? (yes/no) ")
-
         if choice == "yes":
             print("You take the briefcase.")
             character = self.choose_character()
@@ -279,6 +301,7 @@ what?
             print("You don't take the briefcase.")
             # Continues the story without trigger the ambush
             self.continue_story()
+
 
 class SkillCheck:
     """
@@ -302,10 +325,16 @@ class SkillCheck:
         Spends a specified number of luck points on a skill check.
 
         Parameters:
-        luck_points (int): The number of luck points to spend on the skill check.
+        luck_points (int): 
+        The number of luck points to spend on the skill check.
         """
         while True:
-            luck_points = int(input(f'Use LUCK {self.character.lucky_pool}/{self.character.stats["luck"]} '))
+            luck_points = int(
+                input(
+                      f'Use LUCK {self.character.lucky_pool}'
+                      f'/{self.character.stats["luck"]} '
+                      )
+            )
             if luck_points > self.character.lucky_pool:
                 print("Not enough luck points!")
             else:
@@ -318,21 +347,24 @@ class SkillCheck:
 
     def perform_check(self, skill_name, difficulty_value, luck_points):
         """
-        Perform a skill check using a specified skill and difficulty level.
+        Perform a skill check using a specified skill and difficulty
+        level.
 
         Args:
         skill_name (str): The name of the skill to use for the check.
         difficulty_value (str): The difficulty level of the check.
-        luck_points (int): The number of luck points to use for the check.
+        luck_points (int): The number of luck points to use for the
+        check.
 
         Returns:
         None
         """
-
         # Generate a random number from 1 to 10
         d10_roll = random.randint(1, 10)
         # Add d10 roll to total skill level
-        skill_check_total = self.character.skill_total(skill_name) + d10_roll + luck_points
+        skill_check_total = self.character.skill_total(skill_name) \
+                          + d10_roll \
+                          + luck_points
         if d10_roll == 10:
             print("Critical Success! Rolling another one")
             # Generate another random number from 1 to 10
@@ -341,15 +373,17 @@ class SkillCheck:
             print("Critical Failure! Rolling another one")
             # Generate another random number from 1 to 10
             skill_check_total -= random.randint(1,10)
-
         # Get the DV for the specified difficulty level
         difficulty_value = DIFFICULTY_VALUE[difficulty_value]
         if skill_check_total > difficulty_value:
-            print(f"Success! Attacker roll: {skill_check_total}, Defender DV: {difficulty_value}")
+            print(f"Success! Attacker roll: {skill_check_total}, "
+                  f"Defender DV: {difficulty_value}")
         elif skill_check_total < difficulty_value:
-            print(f"Failure! Attacker roll: {skill_check_total}, Defender DV: {difficulty_value}")
+            print(f"Failure! Attacker roll: {skill_check_total}, "
+                  f"Defender DV: {difficulty_value}")
         else:
-            print(f"Tie! Attacker roll: {skill_check_total}, Defender DV: {difficulty_value}")
+            wprint(f"Tie! Attacker roll: {skill_check_total}, "
+                   f"Defender DV: {difficulty_value}")
             print("Attacker loses.")
 
 # Open a shelve in read mode
