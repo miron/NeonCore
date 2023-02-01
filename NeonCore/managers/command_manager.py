ba12503c132b26eaf0567dea0fdea83f13b6afd2
root <m.iron@icloud.com>
2023-02-01 01:01:06 +0000

    do_* methods in classes

--- NeonCore/managers/command_manager.py
+++ NeonCore/managers/command_manager.py
@@ -1,16 +1,13 @@
 from .character_manager  import CharacterManager
+from ..story_modules.phone_call import PhoneCall
 from .action_manager  import ActionManager
 from ..game_maps.game_map import Map
 from ..utils.utils import wprint
 
 
 class CommandManager:
-    #test instance variable vs class
-    commands = {
-        'choose_character': ['do_choose_character',
-                             'complete_choose_character',
-                             'roles'],
-        'character_chosen': ['do_player_sheet', 'do_rap_sheet']}
+    #commands = {
+    #    'phone_call': ['do_phone_call']}
 
     def check_state(self):
         """Check current game state and return commands that should be 
@@ -26,16 +23,14 @@ class CommandManager:
     #            getattr(value['class'], method))
 
     def get_check_command(self):
-        #print(self.game_state)
-        #print(self.cmd_mngr.commands)
-        if self.game_state in self.cmd_mngr.commands:
-            commands = self.cmd_mngr.commands[self.game_state]
-            for command in commands:
-                command_method = getattr(self.cmd_mngr, command)
-                #print(command)
-                setattr(ActionManager, command, command_method) 
-            #print((commands[0][3:]))
-            return [commands[0][3:]]
+        if self.game_state in CharacterManager.commands:
+            commands = CharacterManager.commands[self.game_state]
+        elif self.game_state in PhoneCall.commands:
+            commands = PhoneCall.commands[self.game_state]
+        for command in commands:
+            command_method = getattr(CharacterManager, command)
+            setattr(ActionManager, command, command_method) 
+        return [commands[0][3:]]
         #if self.game_state == 'before_perception_check':
         #    use_skill = SkillCheckCommand(self.player)
         #    use_skill.register_command(self)
@@ -44,112 +39,6 @@ class CommandManager:
         #    pass
         #elif self.game_state == 'before_ranged_combat':
         #    return RangedCombatCommand(self.player, self.npcs)
-        return 
-
-    def roles(self, text=''):
-        return [
-            c.role.lower() for c in 
-            self.char_mngr.characters.values()] if not text else [
-                c.role.lower() for c in 
-                self.char_mngr.characters.values()  if 
-                c.role.lower().startswith(text)]
-
-    def do_choose_character(self, arg=None):
-        """Allows the player to choose a character role."""
-        #print(arg)
-        #breakpoint()
-        if arg not in self.roles():
-            characters_list = [
-                f"{character.handle} ({character.role})" for  character in 
-                self.char_mngr.characters.values()]
-            self.columnize(characters_list, displaywidth=80)
-            wprint(f"To pick yo' ride chummer, type in {self.roles()}.")
-            return
-        self.prompt = f"{arg} >>> "
-        self.player = next(
-            c for c in self.char_mngr.characters.values()  if 
-            c.role.lower() == arg)
-        self.npcs = [
-            c for c in self.char_mngr.characters.values() if 
-            c.role.lower() != arg]
-        self.game_state = 'character_chosen'
-
-    def complete_choose_character(self, text, line, begidx, endidx):
-        return self.roles(text)
-
-    def do_player_sheet(self, arg):
-        """Displays the character sheet"""
-        print(f"HANDLE \033[1;3;35m{self.player.handle:⌁^33}\033[0m ROLE "
-              f"\033[1;3;35m{self.player.role:⌁^33}\033[0m")
-        stat_list = [(f'{key:⌁<12}{self.player.lucky_pool}/{value}' 
-                      if key == 'luck' else f'{key:⌁<12}{value:>2}')
-                     for key, value in self.player.stats.items()]
-        self.columnize(stat_list, displaywidth=80)
-        combat_list = [(f'{key:⌁<23}{value:>2}')
-                        for key, value in self.player.combat.items()]
-        self.columnize(combat_list, displaywidth=80)
-        skill_keys = list(self.player.skills.keys())
-        skill_values = list(self.player.skills.values())
-        skill_list = [(f'{key:⌁<30}{value[0]:>2}')
-                      for key, value in zip(skill_keys,skill_values)
-                      if value[1!=0]]
-        skill_list += self.player.ascii_art.splitlines()
-        self.columnize(skill_list, displaywidth=80)
-        # Display armor & weapons
-        defence_list = (
-            [f"WEAPONS & ARMOR{'⌁'*19:<10} "] 
-            + [' '.join(self.player.defence.keys())] 
-            + [' '.join([str(row) for row in 
-               self.player.defence.values()])])
-        weapons_list = (
-            [' '.join(self.player.weapons[0].keys())] 
-            + [' '.join([str(val) for val in row.values()]
-                        ) for row in self.player.weapons])
-        for defence, weapon in zip(defence_list, weapons_list):
-            print(defence.ljust(35) + weapon.ljust(45))
-        print("ROLE ABILITY " + "⌁"*14 + " CYBERWARE " + "⌁"*17 + " GEAR "
-              + "⌁"*19)
-        ability_list = list(self.player.role_ability.values())
-        ability_list = [row.splitlines() for row in ability_list]
-        ability_list = [item for sublist in ability_list for item in sublist]
-        ware_list = [value for row in self.player.cyberware for key, value in 
-                     row.items()]
-        ware_list = [row.splitlines() for row in ware_list]
-        ware_list = [item for sublist in ware_list for item in sublist]
-        gear_list = ([' '.join(self.player.gear[0].keys())]
-                     + [' '.join(row.values()) for row in self.player.gear]
-                     + [''])
-        for ability, ware, gear in zip(ability_list, ware_list, gear_list):
-            print(ability.ljust(28) + ware.ljust(28) + gear.ljust(24))
-            #if ability == ability_list[0]:
-            #    ability = "\033[1m" + ability + "\033[0m"
-            #if ware == ware_list[0]:
-            #    ware = "\033[1m" + ware + "\033[0m"
-            #if gear == gear_list[0]:
-            #    gear = "\033[1m" + gear + "\033[0m"
-
-
-    def do_rap_sheet(self, arg):
-        """Yo, dis here's rap_sheet, it's gonna show ya all the deetz on
-ya character's backstory, where they came from, who they know, and what
-they're all about.
-It's like peepin' into they mind, know what I'm sayin'? Gotta know ya 
-homies before ya start runnin' with em, ya feel me?
-"""
-        print("Lifepath:")
-        print("Cultural Region:", self.player.cultural_region)
-        print("Personality:", self.player.personality)
-        print("Clothing Style:", self.player.clothing_style)
-        print("Hairstyle:", self.player.hairstyle)
-        print("Value:", self.player.value)
-        print("Trait:", self.player.trait)
-        print("Original Background:", self.player.original_background)
-        print("Childhood Environment:", self.player.childhood_environment)
-        print("Family Crisis:", self.player.family_crisis)
-        print("Friends:", self.player.friends)
-        print("Enemies:", self.player.enemies)
-        print("Lovers:", self.player.lovers)
-        print("Life Goals:", self.player.life_goal)
 
 
 if __name__ == '__main__':
