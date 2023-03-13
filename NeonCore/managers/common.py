6ca98854f1b5a7ba98e80a37119c1b7d1a821385
miron <m.iron@icloud.com>
2023-03-13 22:39:50 +0000

    common module

---
+++ NeonCore/managers/common.py
@@ -0,0 +1,16 @@
+from . import CharacterManager
+from ..game_mechanics import SkillCheckCommand
+from ..story_modules import PhoneCall
+from ..game_maps import  Map
+commands = {
+    'choose_character': ['CharacterManager.do_choose_character',
+                         'CharacterManager.complete_choose_character',
+                         'CharacterManager.roles'],
+    'character_chosen': ['CharacterManager.do_player_sheet',
+                         'CharacterManager.do_rap_sheet',
+                         'PhoneCall.do_phone_call'],
+    'before_perception_check': [
+            'SkillCheckCommand.do_use_skill',
+            'SkillCheckCommand.complete_use_skill',
+            'Map.do_move']}
+
